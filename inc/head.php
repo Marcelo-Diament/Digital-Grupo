ec4f86567de00af2d5dba4e4bd77dca4e2653a3b
hectorqrs <39605920+hectorqrs@users.noreply.github.com>
2018-06-12 16:52:31 -0300

    transformando-o-head
    mudando de php para html

--- inc/head.php
+++ inc/head.php
@@ -1,3 +1,9 @@
-<?php
-<link rel="stylesheet" href="assets/css/style.css">
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <title>Nosso site</title>
+  </head>
