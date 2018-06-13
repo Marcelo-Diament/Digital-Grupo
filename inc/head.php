5902e09e14b309e7c95f5c7cdabb4f68a587fc98
Marcelo Diament <39604367+Marcelo-Diament@users.noreply.github.com>
2018-06-13 17:41:44 -0300

    update head.php com meta data

--- inc/head.php
+++ inc/head.php
@@ -2,10 +2,17 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1,width=device-width,user-scalable=no">
     <link rel="stylesheet" href="/Digital-Grupo/assets/css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <meta name="title" content="Nosso Site">
+    <meta name="author" content="Digital Grupo">
+    <meta name="decription" content="Template de ecommerce desenvolvido com fins didáticos, parte do projeto integrador do curso de Full Stack da Digital House Brasil">
+    <meta name="keywords" content="html, css, php, js, digital house, digital grupo, projeto integrador">
+    <link rel="icon" type="image/png" sizes="96x96" href="https://br.digitalhouse.com/wp-content/themes/dh/assets/img/icons/favicon-96x96.png">
+    <meta name="theme-color" content="#4285f4">
+    <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
     <title>Nosso site</title>
   </head>
 <body>
