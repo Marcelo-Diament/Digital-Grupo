f3adde52f4d36ea185ae236e1c965ba6a1e76f50
hectorqrs <39605920+hectorqrs@users.noreply.github.com>
2018-06-13 18:18:31 -0300

    Adicionando coisas ao footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,19 @@
-<footer>
+<footer row>
+  <div class="newsletter">
+    <h4>Assine nosso Newsletter</h4>
+    <form class="newsletter-box" action="informacoes.php" method="post">
+      <label>
+      <input type="email" name="email-newsletter" placeholder="Insira seu email aqui">
+      <button type="submit" name="submit-newsletter">Ok</button>
+      </label>
+    </form>
+  </div>
+  <div class="contato">
+    <h4>Siga-nos</h4>
+    <a href="#">
+    
+    </a>
+  </div>
 </footer>
 </body>
 </html>
