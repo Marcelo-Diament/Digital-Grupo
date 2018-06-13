3311d767735594388fe19b29b58fa515d45ddbab
hectorqrs <39605920+hectorqrs@users.noreply.github.com>
2018-06-13 19:58:22 -0300

    footer-and-register
    Criei o register.php sem nada dentro e finalizei a parte php do footer sem css.

--- inc/footer.php
+++ inc/footer.php
@@ -8,7 +8,7 @@
       </label>
     </form>
   </div>
-  <div class="contato">
+  <div class="redes-socias">
     <h4>Siga-nos</h4>
     <a href="#">
     <img src="assets/images/logo-facebook.png" alt="Logo do Facebook">
@@ -26,6 +26,34 @@
     <img src="assets/images/logo-youtube.png" alt="Logo do Youtube">
     </a>
   </div>
+  <div class="contatos">
+    <h4>Entre em contato</h4><br>
+    <img src="assets/images/logo-whatsapp.png" alt="Logo do Whatsapp">
+    <a href="tel:+5511987654321">9 8765-4321</a><br>
+    <img src="assets/images/envelope.png" alt="Foto de Envelope">
+    <a href="mailto:hectorqrs@gmail.com?Subject=Contato">hectorqrs@gmail.com</a>
+  </div>
+  <div class="categorias">
+    <h4>Categorias</h4><br>
+    <p>Promoções</p>
+    <ul>
+      <li><a href="#">Categoria #1</a></li>
+      <li><a href="#">Categoria #2</a></li>
+      <li><a href="#">Categoria #3</a></li>
+      <li><a href="#">Categoria #4</a></li>
+    </ul>
+  </div>
+  <div class="institucional">
+    <h4>Institucional</h4><br>
+    <ul>
+      <li><a href="#">Sobre</a></li>
+      <li><a href="#">FAQ</a></li>
+      <li><a href="#">Contato</a></li>
+    </ul>
+  </div>
+  <a href="#">Políticas de Troca e Devolução</a><br>
+  <a href="#">Políticas de Privacidade</a>
+  <p>Direitos Reservados 2018 | CNPJ 99.999.999/0001-99</p>
 </footer>
 </body>
 </html>
