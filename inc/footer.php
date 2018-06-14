aaa5086efb77efae5fce83731f4e3d9dece1ca62
Lucas Palmieri <lucas.palmieri@hotmail.com>
2018-06-14 18:52:36 -0300

    mudancas
    mudancas 14/06 18:52

--- inc/footer.php
+++ inc/footer.php
@@ -1,30 +1,34 @@
-<footer>
-  <div class="newsletter">
-    <h4>Assine nosso Newsletter</h4>
-    <form class="newsletter-box" action="informacoes.php" method="post">
-      <label>
-      <input type="email" name="email-newsletter" placeholder="Insira seu email aqui">
-      <button type="submit" name="submit-newsletter">Ok</button>
-      </label>
-    </form>
-  </div>
-  <div class="redes-socias">
-    <h4>Siga-nos</h4>
-    <a href="#">
-    <img src="assets/images/logo-facebook.png" alt="Logo do Facebook" class="img-redes">
-    </a>
-    <a href="#">
-    <img src="assets/images/logo-instagram.png" alt="Logo do Instagram" class="img-redes">
-    </a>
-    <a href="#">
-    <img src="assets/images/logo-linkedin.png" alt="Logo do Linkedin" class="img-redes">
-    </a>
-    <a href="#">
-    <img src="assets/images/logo-twitter.png" alt="Logo do Twitter" class="img-redes">
-    </a>
-    <a href="#">
-    <img src="assets/images/logo-youtube.png" alt="Logo do Youtube" class="img-redes">
-    </a>
+<footer class="container-fluid">
+  <div class="row">
+   <div class="newsletter col-md-6">
+     <h4>Assine nosso Newsletter</h4>
+     <form class="newsletter-box" action="informacoes.php" method="post">
+       <label>
+       <input type="email" name="email-newsletter" placeholder="Insira seu email aqui">
+       <button type="submit" name="submit-newsletter">Ok</button>
+       </label>
+     </form>
+   </div>
+   <div class="redes-socias col-md-6">
+      <h4>Siga-nos</h4>
+      <div class="row">
+        <a href="#" class="offset-1 col-2">
+          <img src="assets/images/logo-facebook.png" alt="Logo do Facebook" class="img-redes">
+        </a>
+        <a href="#" class="col-2">
+          <img src="assets/images/logo-instagram.png" alt="Logo do Instagram" class="img-redes">
+        </a>
+        <a href="#" class="col-2">
+          <img src="assets/images/logo-linkedin.png" alt="Logo do Linkedin" class="img-redes">
+        </a>
+        <a href="#" class="col-2">
+          <img src="assets/images/logo-twitter.png" alt="Logo do Twitter" class="img-redes">
+        </a>
+        <a href="#" class="col-2">
+          <img src="assets/images/logo-youtube.png" alt="Logo do Youtube" class="img-redes">
+        </a>
+      </div>
+    </div>
   </div>
   <div class="contatos">
     <h4>Entre em contato</h4><br>
@@ -49,11 +53,11 @@
       <li><a href="#">Sobre</a></li>
       <li><a href="#">FAQ</a></li>
       <li><a href="#">Contato</a></li>
+      <li><a href="#">Políticas de Troca e Devolução</a></li>
+      <li><a href="#">Políticas de Privacidade</a></li>
     </ul>
   </div>
-  <a href="#">Políticas de Troca e Devolução</a><br>
-  <a href="#">Políticas de Privacidade</a>
-  <p>Direitos Reservados 2018 | CNPJ 99.999.999/0001-99</p>
+  <small style="color: white;">Direitos Reservados Marca &copy; <?php echo date("Y")?> | CNPJ 99.999.999/0001-99</small>
 </footer>
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,35 +2,37 @@
 include ("inc/head.php");
 include ("inc/header.php");
 ?>
-<div class="container-fluid">
-  <div class="div_banner col-lg-12">
-    <h2>Título do Banner</h2>
-      <p>Subtitulo do Banner até 2 linhas!</p>
-      <p><a class="vermais1">Ver Mais</a></p>
-  </div>
-  <div class="div_produtos row">
-     <section class="produto1 col-sm-4">
-       <h3>Nome do Produto</h3>
-       <p><b>12x </b>$499,99 <b>sem juros</b></p>
-       <p><a class="vermais alinhar">Ver Mais</a></p>
-     </section>
-     <section class="produto2 col-sm-4">
-       <h3>Nome do Produto</h3>
-       <p><b>12x </b>$499,99 <b>sem juros</b></p>
-       <p><a class="vermais">Ver Mais</a></p>
-     </section>
-     <section class="produto3 col-sm-4">
-       <h3>Nome do Produto</h3>
-       <p><b>12x </b>$499,99 <b>sem juros</b></p>
-       <p><a class="vermais">Ver Mais</a></p>
-     </section>
-     <section class="produto4 col-sm-4">
-       <h3>Nome do Produto</h3>
-       <p><b>12x </b>$499,99 <b>sem juros</b></p>
-       <p><a class="vermais">Ver Mais</a></p>
-     </section>
-   </div>
- </div>
+  <main>
+    <div class="container-fluid">
+      <div class="div_banner col-lg-12">
+        <h2>Título do Banner</h2>
+          <p>Subtitulo do Banner até 2 linhas!</p>
+          <p><a class="vermais1">Ver Mais</a></p>
+      </div>
+      <div class="div_produtos row">
+         <section class="produto1 col-sm-6">
+           <h3>Nome do Produto</h3>
+           <p><b>12x </b>$499,99 <b>sem juros</b></p>
+           <p><a class="vermais alinhar">Ver Mais</a></p>
+         </section>
+         <section class="produto2 col-sm-6">
+           <h3>Nome do Produto</h3>
+           <p><b>12x </b>$499,99 <b>sem juros</b></p>
+           <p><a class="vermais">Ver Mais</a></p>
+         </section>
+         <section class="produto3 col-sm-6">
+           <h3>Nome do Produto</h3>
+           <p><b>12x </b>$499,99 <b>sem juros</b></p>
+           <p><a class="vermais">Ver Mais</a></p>
+         </section>
+         <section class="produto4 col-sm-6">
+           <h3>Nome do Produto</h3>
+           <p><b>12x </b>$499,99 <b>sem juros</b></p>
+           <p><a class="vermais">Ver Mais</a></p>
+         </section>
+       </div>
+     </div>
+   </main>
 <?php
 include ("inc/footer.php");
  ?>
