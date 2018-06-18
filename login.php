87c5953b6cdb71ed704aaad32bf5913745d28096
hectorqrs <39605920+hectorqrs@users.noreply.github.com>
2018-06-18 17:20:15 -0300

    login-regiter
    Adicionei o bootstrap às caixas de login e cadastro.

--- login.php
+++ login.php
@@ -2,15 +2,36 @@
 include ("inc/head.php");
 include ("inc/header.php");
 ?>
-<div class="conteiner-login">
 <form class="login" action="informacoes.php" method="post">
-  <input type="email" name="email" placeholder="E-mail"><br>
-  <input type="password" name="senha" placeholder="Senha"><br>
-  <input type="submit" name="enviar" value="Logar"><br>
+  <div class="form-group">
+    <input type="email" name="email" placeholder="E-mail">
+  </div>
+  <div class="form-group">
+    <input type="password" name="senha" placeholder="Senha">
+    <small class="form-text text-muted">Vamos roubar a sua senha</small>
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input">
+    <label class="form-check-label">Permanecer logado</label>
+  </div>
+  <div>
+    <input class="btn btn-primary" type="submit" name="enviar" value="Logar">
+  </div>
 </form>
-<p>ou
-</p>
-  <button type="button" name="button">Entrar com Facebook</button>
+<p>ou</p>
+  <button type="button" name="button">Entrar com Facebook</button><br>
+  <a href="register.php">Criar uma conta</a>
+<div class="footer-links">
+  <a href="#">FAQ</a>
+  <span>|</span>
+  <a href="#">Contatos</a>
+  <span>|</span>
+  <a href="#">Políticas de Troca e Devolução</a>
+  <span>|</span>
+  <a href="#">Políticas de Privacidade</a>
 </div>
-
-<?php include("inc/footer.php") ?>
+<div class="copyright">
+  ©2018 Digital-Grupo, INC. TODOS OS DIREITOS RESERVADOS A Hector Queiróz.
+</div>
+</body>
+</head>
--- register.php
+++ register.php
@@ -2,23 +2,35 @@
 include ("inc/header.php") ?>
   <form class="" action="informacoes.php" method="post">
     <section>
-      <label>Nome Completo: </label>
-      <input placeholder="Nome Completo" type="text" name="Nome" required><br>
-      <label>CPF: </label>
-      <input placeholder="111.222.333-44" type="number" name="CPF" required><br>
-      <label>E-mail: </label>
-      <input placeholder="seu-email@email.com" type="email" name="E-mail" required><br>
-      <label>Telefone: </label>
-      <input placeholder="(11)99999-9999" type="tel" name="Telefone" required><br>
-      <label>Senha: </label>
-      <input placeholder="*********" type="password" name="Senha" required><br>
+      <div class="form-group">
+        <label>Nome Completo: </label>
+        <input placeholder="Nome Completo" type="text" name="Nome" required>
+      </div>
+      <div class="form-group">
+        <label>CPF: </label>
+        <input placeholder="111.222.333-44" type="number" name="CPF" required>
+      </div>
+      <div class="form-group">
+        <label>E-mail: </label>
+        <input placeholder="seu-email@email.com" type="email" name="E-mail" required>
+      </div>
+      <div class="form-group">
+        <label>Telefone: </label>
+        <input placeholder="(11)99999-9999" type="tel" name="Telefone" required>
+      </div>
+      <div class="form-group">
+        <label>Senha: </label>
+        <input placeholder="*********" type="password" name="Senha" required>
+      </div>
       <label>Sexo: </label><br>
       <input type="radio" name="Sexo" value="Masculino">
       <label>Masculino </label>
       <input type="radio" name="Sexo" value="Feminino">
       <label>Feminino </label><br>
-      <input type="checkbox" name="promocoes">
-      <label>Desejo receber novidades sobre as promoções</label><br>
+      <div class="form-group">
+        <input type="checkbox" class="form-check-input" name="promocoes">
+        <label class="form-check-label">Desejo receber novidades sobre as promoções</label>
+      </div>
       <input type="submit" name="enviar" value="Finalizar Cadastro">
       <input type="reset" name="limpar" value="Limpar"><br
       <label>Possui uma conta? </label>
