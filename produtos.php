e950dc6675d5b129666bb1226a6c57394d19ccfd
Marcelo-Diament <marcelodiament@gmail.com>
2018-06-19 19:00:28 -0300

    pagina-produtos-e-novo-style
    Style novo para trabalhar em paralelo ao Hector. Página de Vitrine de Produtos.

---
+++ produtos.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1,width=device-width,user-scalable=no">
+    <link rel="stylesheet" href="/Digital-Grupo/assets/css/style-produtos-marcelo.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <meta name="title" content="Nosso Site">
+    <meta name="author" content="Digital Grupo">
+    <meta name="decription" content="Template de ecommerce desenvolvido com fins didáticos, parte do projeto integrador do curso de Full Stack da Digital House Brasil">
+    <meta name="keywords" content="html, css, php, js, digital house, digital grupo, projeto integrador">
+    <link rel="icon" type="image/png" sizes="96x96" href="https://br.digitalhouse.com/wp-content/themes/dh/assets/img/icons/favicon-96x96.png">
+    <meta name="theme-color" content="#4285f4">
+    <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
+    <title>Nosso site</title>
+  </head>
+<body>
+<?php
+include ("inc/header.php");
+?>
+  <main class="container-fluid">
+    <section class="top row">
+      <article class="top-title col-12">
+        <h1>Categoria 01</h1>
+        <h4>Produtos pertencentes à categoria 01</h4>
+      </article>
+      <p><span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"> <a href="http://digitalgrupo.provisorio.ws/" itemprop="url"><span itemprop="title">Home</span></a> › </span><span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="http://digitalgrupo.provisorio.ws/produtos/" itemprop="url"> <span itemprop="title">Produtos</span> </a> › </span>Categoria 01</p>
+    </section>
+    <section class="pdt-vitrine row">
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <span class="cta-banner-horizontal"><a href="#"></a></span>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <article class="pdt-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <h3 class="pdt-card-title">Nome do Produto</h3>
+        <div class="pdt-card-price">12x de R$ 499,00 sem juros</div>
+        <button class="pdt-card-btn btn"><a href="#">Ver Mais</a></button>
+      </article>
+      <span><a href="#" alt="Clique para ver a página 1"><i class="fas fa-angle-double-left"></i></a><a href="#" alt="Clique para ver a página anterior"><i class="fas fa-angle-left"></i></a> Exibindo 16 resultados de 64 resultados <a href="#" alt="Clique para ver a próxima página"><i class="fas fa-angle-right"></i></a><a href="#" alt="Clique para ver a última página"><i class="fas fa-angle-double-right"></i></a></span>
+    </section>
+  </main>
+<?php
+include ("inc/footer.php");
+ ?>
