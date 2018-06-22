60080f891239cb560702c78936d00b2e1b329be5
hectorqrs <39605920+hectorqrs@users.noreply.github.com>
2018-06-22 19:32:21 -0300

    header-and-home
    Ajustando o header tanto para mobile quanto para desktop

--- index.php
+++ index.php
@@ -2,12 +2,27 @@
 include ("inc/head.php");
 include ("inc/header.php");
 ?>
+  <div class="menu-2 col-lg-12">
+    <ul>
+      <li><a href="#">Lançamentos</a></li>
+      <li><a href="#">Promoções</a></li>
+      <li><a href="#">Categoria #1</a></li>
+      <li><a href="#">Categoria #2</a></li>
+      <li><a href="#">Categoria #3</a></li>
+      <li><a href="#">Categoria #4</a></li>
+    </ul>
+  </div>
   <main>
     <div class="container-fluid">
       <div class="div_banner col-lg-12">
         <h2>Título do Banner</h2>
           <p>Subtitulo do Banner até 2 linhas!</p>
-          <p><a class="vermais1">Ver Mais</a></p>
+          <p id="link_banner"><a class="vermais1">Ver Mais</a></p>
+      </div>
+      <div class="destaque col-lg-12">
+        <h3>Nome do Produto</h3>
+        <p>12x de R$ 499.99 sem juros</p>
+        <p id="link_destaque"><a href="#">Ver mais</a><p>
       </div>
       <section class="div_produtos row">
          <article class="produto1 col-sm-5">
