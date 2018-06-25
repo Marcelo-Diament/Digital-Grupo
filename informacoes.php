c444bda937a095989c3f9e54695e27338fabe654
hectorqrs <39605920+hectorqrs@users.noreply.github.com>
2018-06-25 13:33:51 -0300

    register-informacoes
    A página de cadastro não finalizada e login funcional

---
+++ informacoes.php
@@ -0,0 +1,4 @@
+<?php
+echo "Seu email é :".$_POST["email"];
+echo "Sua senha é :".$_POST["senha"];
+ ?>
--- register.php
+++ register.php
@@ -1,40 +1,78 @@
 <?php include ("inc/head.php");
-include ("inc/header.php") ?>
-  <form class="" action="informacoes.php" method="post">
-    <section>
-      <div class="form-group">
-        <label>Nome Completo: </label>
-        <input placeholder="Nome Completo" type="text" name="Nome" required>
+include ("inc/header.php"); ?>
+  <section class="register">
+    <form= action="informacoes.php" method="post">
+      <article class="row">
+        <div class="col-sm-1">
+        </div>
+        <div class="form-group col-sm-5">
+          <label><b>Nome: </b></label>
+          <input class="form-control" placeholder="Nome" type="text" name="Nome" required>
+        </div>
+        <div class="form-group col-sm-5">
+          <label><b>Sobrenome: </b></label>
+          <input class="form-control" placeholder="Sobrenome" type="text" name="Sobrenome" required>
+        </div>
+      </article>
+      <article class="row">
+        <div class="col-sm-1">
+        </div>
+        <div class="form-group col-sm-5">
+          <label><b>E-mail: </b></label>
+          <input class="form-control" placeholder="Ex: seu-email@email.com" type="email" name="E-mail" required>
+        </div>
+        <div class="form-group col-sm-5">
+          <label><b>Confirme seu e-mail: </b></label>
+          <input class="form-control" placeholder="EX: seu-email@email.com" type="email" name="E-mail_confirm" required>
+        </div>
+      </article>
+      <article class="row">
+        <div class="col-sm-1">
+        </div>
+        <div class="form-group col-sm-5">
+          <label><b>Senha: </b></label>
+          <input class="form-control" placeholder="*********" type="password" name="Senha" required>
+        </div>
+        <div class="form-group col-sm-5">
+          <label><b>Confirme sua senha: </b></label>
+          <input class="form-control" placeholder="*********" type="password" name="Senha" required>
+        </div>
+      </article>
+      <article class="row">
+        <div class="col-sm-1">
+        </div>
+        <div class="form-group col-sm-5">
+          <label><b>CPF: </b></label>
+          <input class="form-control" placeholder="123.456.789-00" type="tel" name="telefone" required>
+        </div>
+        <div class="form-group col-sm-5">
+          <label><b>Telefone: </b></label>
+          <input class="form-control" placeholder="1234-5678" type="number" name="cpf" required>
+        </div>
+      </article>
+      <div class="form-group row">
+        <div class="col-sm-1"></div>
+        <label><b>Gênero: </b></label>
+        <div>
+          <label class="radio-inline">
+            <input type="radio" name="genero" value="Masculino"> Masculino
+          </label>
+          <label class="radio-inline">
+            <input type="radio" name="genero" value="Feminino"> Feminino
+          </label>
+        </div>
       </div>
-      <div class="form-group">
-        <label>CPF: </label>
-        <input placeholder="111.222.333-44" type="number" name="CPF" required>
+      <div class="form-group row">
+        <div class="col-sm-1">
+        </div>
+        <label><b>Data de Nascimento: </b></label>
+        <input type="date" name="nascimento" value="">
       </div>
-      <div class="form-group">
-        <label>E-mail: </label>
-        <input placeholder="seu-email@email.com" type="email" name="E-mail" required>
-      </div>
-      <div class="form-group">
-        <label>Telefone: </label>
-        <input placeholder="(11)99999-9999" type="tel" name="Telefone" required>
-      </div>
-      <div class="form-group">
-        <label>Senha: </label>
-        <input placeholder="*********" type="password" name="Senha" required>
-      </div>
-      <label>Sexo: </label><br>
-      <input type="radio" name="Sexo" value="Masculino">
-      <label>Masculino </label>
-      <input type="radio" name="Sexo" value="Feminino">
-      <label>Feminino </label><br>
-      <div class="form-group">
-        <input type="checkbox" class="form-check-input" name="promocoes">
-        <label class="form-check-label">Desejo receber novidades sobre as promoções</label>
-      </div>
-      <input type="submit" name="enviar" value="Finalizar Cadastro">
-      <input type="reset" name="limpar" value="Limpar"><br
+      <input class="teste" type="submit" name="enviar" value="Finalizar Cadastro">
+      <input class="teste" type="reset" name="limpar" value="Limpar"><br>
       <label>Possui uma conta? </label>
       <a href="login.php">Entrar</a>
-    </section>
-  </form>
+    </form>
+  </section>
+
 <?php include ("inc/footer.php") ?>
